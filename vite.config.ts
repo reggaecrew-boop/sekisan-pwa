60f7c035e3c6a1348b89d7fc0b9e59a1b5258bd5
reggaecrew-boop <reggaecrew@gmail.com>
2026-02-18 06:33:06 +0900

    Delete vite.config.ts

--- vite.config.ts
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-
-// GitHub Pages (repo: sekisan-pwa)
-export default defineConfig({
-  plugins: [react()],
-  base: "/sekisan-pwa/",
-});
